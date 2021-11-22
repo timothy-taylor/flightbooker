41a4e7adfd6f3d6aa1f333664bd3deca50e6a874
Tim <timothy.george.taylor@gmail.com>
2021-11-22 14:40:16 -0700

    added airport/flight models and associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+airports = Airport.create([{ code: 'SFO', city: 'San Francisco' }, 
+                           { code: 'NYC', city: 'New York City' }, 
+                           { code: 'ABQ', city: 'Albuquerque' }])
+
+flights = Flight.create([
+  { departure_airport_id: Airport.first.id, 
+    arrival_airport_id: Airport.last.id, 
+    departure_time: Time.now, 
+    duration: 138 },
+  { departure_airport_id: Airport.last.id,
+    arrival_airport_id: Airport.second.id,
+    departure_time: Time.now,
+    duration: 212 },
+  { departure_airport_id: Airport.second.id,
+    arrival_airport_id: Airport.first.id,
+    departure_time: Time.now,
+    duration: 346 }])
