4daa2a345aced3822fa45b4921a15f4a748f0cc8
Tim <timothy.george.taylor@gmail.com>
2021-12-15 18:38:49 -0700

    more style updates

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,18 +1,18 @@
 class Flight < ApplicationRecord
-  belongs_to :arrival_airport, class_name: "Airport" , foreign_key: "arrival_airport_id"
-  belongs_to :departure_airport, class_name: "Airport" , foreign_key: "departure_airport_id"
+  belongs_to :arrival_airport, class_name: 'Airport', foreign_key: 'arrival_airport_id'
+  belongs_to :departure_airport, class_name: 'Airport', foreign_key: 'departure_airport_id'
   has_many :bookings
   has_many :passengers, through: :bookings
 
   def fmt_time_display
-    departure_time.strftime("%m/%d/%Y %I:%M %p")
+    departure_time.strftime('%m/%d/%Y %I:%M %p')
   end
 
   def fmt_time_search
-    departure_time.strftime("%m/%d/%Y")
+    departure_time.strftime('%m/%d/%Y')
   end
 
   def fmt_time_param
-    departure_time.strftime("%m%d%Y")
+    departure_time.strftime('%m%d%Y')
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,53 +1,71 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
 
-airports = Airport.create([{ code: 'SFO', city: 'San Francisco' }, 
-                           { code: 'NYC', city: 'New York City' }, 
-                           { code: 'ABQ', city: 'Albuquerque' },
-                           { code: 'BOS', city: 'Boston' },
-                           { code: 'SEA', city: 'Seattle' },
-                           { code: 'ATL', city: 'Atlanta' }])
+Airport.create(
+  [
+    { code: 'SFO', city: 'San Francisco' },
+    { code: 'NYC', city: 'New York City' },
+    { code: 'ABQ', city: 'Albuquerque' },
+    { code: 'BOS', city: 'Boston' },
+    { code: 'SEA', city: 'Seattle' },
+    { code: 'ATL', city: 'Atlanta' }
+  ]
+)
 
-flights = Flight.create([
-  { departure_airport_id: Airport.first.id, 
-    arrival_airport_id: Airport.third.id, 
-    departure_time: Time.now + 9100000, 
-    duration: 138 },
-  { departure_airport_id: Airport.third.id,
-    arrival_airport_id: Airport.second.id,
-    departure_time: Time.now + 7434,
-    duration: 212 },
-  { departure_airport_id: Airport.second.id,
-    arrival_airport_id: Airport.first.id,
-    departure_time: Time.now + 3248000,
-    duration: 371 },
- { departure_airport_id: Airport.third.id,
-    arrival_airport_id: Airport.first.id,
-    departure_time: Time.now + 88888,
-    duration: 133 },
- { departure_airport_id: Airport.first.id,
-    arrival_airport_id: Airport.second.id,
-    departure_time: Time.now + 888888,
-    duration: 346 },
- { departure_airport_id: Airport.second.id,
-    arrival_airport_id: Airport.first.id,
-    departure_time: Time.now + 8888888,
-    duration: 371 },
- { departure_airport_id: Airport.fourth.id,
-    arrival_airport_id: Airport.fifth.id,
-    departure_time: Time.now + 395888808,
-    duration: 341 },
- { departure_airport_id: Airport.last.id,
-    arrival_airport_id: Airport.fourth.id,
-    departure_time: Time.now + 8888,
-    duration: 140 },
- { departure_airport_id: Airport.last.id,
-    arrival_airport_id: Airport.first.id,
-    departure_time: Time.now + 8888,
-    duration: 303 },
-])
+Flight.create(
+  [
+    {
+      departure_airport_id: Airport.first.id,
+      arrival_airport_id: Airport.third.id,
+      departure_time: Time.now + 9_100_000,
+      duration: 138
+    },
+    {
+      departure_airport_id: Airport.third.id,
+      arrival_airport_id: Airport.second.id,
+      departure_time: Time.now + 7434,
+      duration: 212
+    },
+    {
+      departure_airport_id: Airport.second.id,
+      arrival_airport_id: Airport.first.id,
+      departure_time: Time.now + 3_248_000,
+      duration: 371
+    },
+    {
+      departure_airport_id: Airport.third.id,
+      arrival_airport_id: Airport.first.id,
+      departure_time: Time.now + 88_888,
+      duration: 133
+    },
+    {
+      departure_airport_id: Airport.first.id,
+      arrival_airport_id: Airport.second.id,
+      departure_time: Time.now + 888_888,
+      duration: 346
+    },
+    {
+      departure_airport_id: Airport.second.id,
+      arrival_airport_id: Airport.first.id,
+      departure_time: Time.now + 8_888_888,
+      duration: 371
+    },
+    {
+      departure_airport_id: Airport.fourth.id,
+      arrival_airport_id: Airport.fifth.id,
+      departure_time: Time.now + 395_888_808,
+      duration: 341
+    },
+    {
+      departure_airport_id: Airport.last.id,
+      arrival_airport_id: Airport.fourth.id,
+      departure_time: Time.now + 8888,
+      duration: 140
+    },
+    {
+      departure_airport_id: Airport.last.id,
+      arrival_airport_id: Airport.first.id,
+      departure_time: Time.now + 8888,
+      duration: 303
+    }
+  ]
+)
